cdd2fe64fe511d89f59936b2f2e6ef6adc754d77
srodney <stevenrodney@hotmail.com>
2021-04-30 10:59:19 +0100

    Final file for exercise 1.0.1

--- rust/01_SayingHello/saying_hello_v01/src/main.rs
+++ rust/01_SayingHello/saying_hello_v01/src/main.rs
@@ -1,30 +1,29 @@
 use std::io;
+use std::io::Write;
 
 pub(crate) fn main() {
-    let mut question = String::from("What is your name?");
-    trim_newline(&mut question);
-
-    max_point();
-
-    println!("{}", question);
+    //--------------------------------------------------------------------------
+    // println! adds a newline character to the end of string so that the input 
+    // cursor is on next line. Using print! solves the issue but stdout is 
+    // frequently line-buffered by default so it may be necessary to use 
+    // io::stdout().flush() to ensure the output is emitted immediately.
+    //--------------------------------------------------------------------------
+    print!("What is your name? ");
+    io::stdout().flush().unwrap();
     let mut my_name = String::new();
 
     io::stdin()
     .read_line(&mut my_name)
     .expect("Failed to read line");
-
     trim_newline(&mut my_name);
 
     println!("Hello {}, nice to meet you!", my_name);
 }
-
+//------------------------------------------------------------------------------
+// Removes newline characters if they are found at the end of strings
+//------------------------------------------------------------------------------
 fn trim_newline(s: &mut String) {
     while s.ends_with('\n') || s.ends_with('\r') {
         s.pop();
     }
-}
-
-fn max_point() -> i32 {
-    const MAX_POINT: i32 = 100;
-    MAX_POINT
 }
